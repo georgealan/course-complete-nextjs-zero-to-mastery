f2df692858edeffb4188f883b20a0bb2418a2182
George Alan <georgealanrufo@gmail.com>
2021-12-11 18:25:46 -0300

    Módulo 7 Aula 69, inicio da conexão com API externa Foursquare develpers

--- Course-Projects/discover-coffee-stores/pages/coffee-store/[id].js
+++ Course-Projects/discover-coffee-stores/pages/coffee-store/[id].js
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/jsx-no-undef */
 import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 import Link from 'next/link';
+import Image from "next/image";
+import styles from '../../styles/coffee-store.module.css';
+import cls from "classnames";
 
 import coffeeStoresData from '../../data/coffee-stores.json';
 
+/** Server side function */
 export function getStaticProps(staticProps) {
     const params = staticProps.params;
 
@@ -16,13 +22,23 @@ export function getStaticProps(staticProps) {
     };
 }
 
+/** Server side function */
 export function getStaticPaths() {
+    const paths = coffeeStoresData.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString(),
+            },
+        };
+    });
+
     return {
-        paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+        paths,
         fallback: true,
     };
 }
 
+/** Client side */
 const CoffeeStore = (props) => {
     const router = useRouter();
 
@@ -30,15 +46,59 @@ const CoffeeStore = (props) => {
         return <div>Loading...</div>
     }
 
-    return (
-        <div>
-            Coffee Store Page {router.query.id}
-            <Link href="/">
-                <a>Back to home</a>
-            </Link>
+    /* Desestructuration */
+    const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
 
-            <p>{props.coffeeStore.address}</p>
-            <p>{props.coffeeStore.name}</p>
+    const handleUpVoteButton = () => {
+        console.log("handle upvote");
+    };
+
+    return (
+        <div className={styles.layout}>
+            <Head>
+                <title>{name}</title>
+            </Head>
+
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="/">
+                            <a>Back to home</a>
+                        </Link>
+                    </div>
+
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+
+                    <Image 
+                        src={imgUrl}
+                        width={600}
+                        height={360}
+                        className={styles.storeImg}
+                        alt={name}
+                    />
+                </div>
+
+                <div className={cls("glass", styles.col2)}>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/places.svg" width="24" height="24" />
+                        <p className={styles.text}>{address}</p>
+                    </div>
+
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+                        <p className={styles.text}>{neighbourhood}</p>
+                    </div>
+
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/star.svg" width="24" height="24" />
+                        <p className={styles.text}>1</p>
+                    </div>
+
+                    <button className={styles.upvoteButton} onClick={handleUpVoteButton}>Up vote!</button>
+                </div>
+            </div>
         </div>
     );
 };
